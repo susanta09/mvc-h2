6e413cf853dcc837bf835a30b6ca2ecd7a7fc2a6
susanta <ssbarmant107@gmail.com>
2023-09-03 23:20:54 -1200

    update the project mvc-h2 using bean validation

--- src/main/java/com/example/controller/EmpController.java
+++ src/main/java/com/example/controller/EmpController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.entity.Employee;
 import com.example.service.EmpService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class EmpController {
@@ -29,9 +32,13 @@ public class EmpController {
 		return "form";
 	}
 	@PostMapping("/emp")
-	public String getUiData(Employee employee,Model m)
+	public String getUiData(@Valid Employee employee,BindingResult result,Model m)
 	{
-		System.out.println(employee);
+		if(result.hasErrors())
+		{
+			return "form";
+		}
+		System.out.println("save "+employee);
 		Boolean b=empService.saveData(employee);
 		
 		if(b)
--- src/main/java/com/example/entity/Employee.java
+++ src/main/java/com/example/entity/Employee.java
@@ -1,9 +1,16 @@
 package com.example.entity;
 
+
+import jakarta.annotation.security.PermitAll;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,8 +23,17 @@ public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer empId;
+	@NotEmpty(message = "Name can not be empty")
+	@Pattern(regexp = "^[A-Z][a-z]+\\W[A-Z][a-z]+$",
+			  message ="First letter of String Must be capital.Take a space between two string")
 	private String empName;
+	
+	@NotNull(message = "Age can not be empty")
+	@Min(value = 18,message = "Age must be above 18.")
+	@Max(value = 70,message =" below 70")
 	private Integer empAge;
+	@NotNull(message = "Age can not be empty")
+	@Min(value = 10000, message = "Minimum salary is 10,000")
 	private Double empSal;
 	
 }
